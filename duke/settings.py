53f3b8a01b08975f7c028dc40ed32b835952c2e3
Basweti <baswetima@gmail.com>
2019-07-29 07:50:15 +0300

    add local settings

--- duke/settings.py
+++ duke/settings.py
@@ -162,3 +162,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'baswetima@gmail.com'
 EMAIL_HOST_PASSWORD = 'trizabas2017'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
